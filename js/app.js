06d6c06fd253ca4d1a6841183c6c8ee268c99e89
antipenko <sweetglory.victory@gmail.com>
2016-10-02 16:38:02 +0300

    add adaptive for mobile

--- js/app.js
+++ js/app.js
@@ -1,1 +1,6 @@
 $(document).foundation();
+
+document.getElementById('designer').addEventListener('click', function(){
+	var n = document.getElementsByClassName('designer');
+	n.innerHTML = '[eq[qe';
+});
