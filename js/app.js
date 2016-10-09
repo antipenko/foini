a8b96ca00eae95407556a6181b731c08e90909eb
antipenko <sweetglory.victory@gmail.com>
2016-10-09 18:15:43 +0300

    add bem

--- js/app.js
+++ js/app.js
@@ -1,6 +1,23 @@
 $(document).foundation();
 
+// document.getElementById('designer').addEventListener('click', function(){
+// 	var n = document.getElementsByClassName('designer');
+// 	n.innerHTML = '[eq[qe';
+// });
+
+//  $(document).ready(function(){
+//     $("#menu").on("click","a", function (event) {
+//         event.preventDefault();
+//         var id  = $(this).attr('href');
+//         	console.log(id);
+//             top = $(id).offset().top;
+//         $('body,html').animate({scrollTop: top}, 1500);
+//     });
+// });
+
 document.getElementById('designer').addEventListener('click', function(){
-	var n = document.getElementsByClassName('designer');
-	n.innerHTML = '[eq[qe';
+	var design = document.getElementsByClassName('designer');
+	var works = document.getElementsByClassName('mix');
+	works.hidden;
+	console.log(design);
 });
